5ff35f340bcc16dc750892fc8b2129b6dded98a3
Cayge <caygesinnett@gmail.com>
2023-04-05 08:52:09 -0500

    need to understand css positioning so that rotation, schedule, etc... divs are not on top of each other, added grey bubble border

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import logo from './logo.svg'
+import './App.css'
 
 function App() {
 	return (
@@ -13,19 +14,19 @@ function App() {
 }
 
 function Rotation() {
-	return <div id="rotation"></div>
+	return <div id="rotation" className="squareBubble"></div>
 }
 
 function Schedule() {
-	return <div id="schedule"></div>
+	return <div id="schedule" className="squareBubble"></div>
 }
 
 function Cut() {
-	return <div id="cut"></div>
+	return <div id="cut" className="squareBubble"></div>
 }
 
 function Introduce() {
-	return <div id="introduce"></div>
+	return <div id="introduce" className="squareBubble"></div>
 }
 
 export default App
