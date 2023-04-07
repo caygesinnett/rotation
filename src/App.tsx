d19cf852132e89d5f188df9763d5409023d5f42a
Cayge <caygesinnett@gmail.com>
2023-04-07 00:19:14 -0500

    cursor is pointer on hover

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,50 @@ import logo from './logo.svg'
 import './app.css'
 
 function App() {
+	const [isOpen, setIsOpen] = useState(false)
+
+	function Rotation() {
+		return (
+			<div id="rotation" className="squareBubble">
+				<div className="bubbleLabel">Rotation</div>
+			</div>
+		)
+	}
+
+	function Schedule() {
+		return (
+			<div id="schedule" className="squareBubble">
+				<div className="bubbleLabel">Scheduled Servers</div>
+			</div>
+		)
+	}
+
+	function Cut() {
+		return (
+			<div id="cut" className="squareBubble">
+				<div className="bubbleLabel">Cut Servers</div>
+			</div>
+		)
+	}
+
+	function Introduce() {
+		return (
+			<div id="introduce" className="squareBubble">
+				<div className="bubbleLabel">Add New Servers</div>
+				<Open />
+			</div>
+		)
+	}
+
+	function Open() {
+		const openOrClose = () => (isOpen ? setIsOpen(false) : setIsOpen(true))
+		return (
+			<div className="block centerText" onClick={openOrClose}>
+				{isOpen ? 'Close' : 'Add Server to Schedule'}
+			</div>
+		)
+	}
+
 	return (
 		<>
 			<Rotation />
@@ -10,49 +54,6 @@ function App() {
 			<Cut />
 			<Introduce />
 		</>
-	)
-}
-
-function Rotation() {
-	return (
-		<div id="rotation" className="squareBubble">
-			<div className="bubbleLabel">Rotation</div>
-		</div>
-	)
-}
-
-function Schedule() {
-	return (
-		<div id="schedule" className="squareBubble">
-			<div className="bubbleLabel">Scheduled Servers</div>
-		</div>
-	)
-}
-
-function Cut() {
-	return (
-		<div id="cut" className="squareBubble">
-			<div className="bubbleLabel">Cut Servers</div>
-		</div>
-	)
-}
-
-function Introduce() {
-	return (
-		<div id="introduce" className="squareBubble">
-			<div className="bubbleLabel">Add New Servers</div>
-			<Open />
-		</div>
-	)
-}
-
-function Open() {
-	const [isOpen, setIsOpen] = useState(false)
-	const openOrClose = () => (isOpen ? setIsOpen(false) : setIsOpen(true))
-	return (
-		<div className="block centerText" onClick={openOrClose}>
-			{isOpen ? 'Close' : 'Add Server to Schedule'}
-		</div>
 	)
 }
 
