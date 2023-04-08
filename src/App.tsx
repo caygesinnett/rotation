0b42aef724d4e12c0bd712df6e81ccb1df094839
Cayge <caygesinnett@gmail.com>
2023-04-08 11:30:02 -0500

    able to add servers to the Add New Servers section

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
-import React, { useState } from 'react'
+import React, { Children, useState } from 'react'
 import logo from './logo.svg'
 import './app.css'
 
 function App() {
-	const [isOpen, setIsOpen] = useState(false)
+	const [optionsAreOpen, setOptionsAreOpen] = useState(false)
+	const [serverOptions, setServerOptions] = useState<Array<any>>([])
+	const [newServerIsOpen, setNewServerIsOpen] = useState(false)
+
+	return (
+		<>
+			<Rotation />
+			<Schedule />
+			<Cut />
+			<Introduce>
+				{optionsAreOpen ? (
+					<>
+						<CloseOptions />
+						{serverOptions.map((server, index) => {
+							return <ServerOption key={index.toString()}>{server.name}</ServerOption>
+						})}
+						{newServerIsOpen ? <NewServer /> : <AddServer />}
+					</>
+				) : (
+					<OpenOptions />
+				)}
+			</Introduce>
+		</>
+	)
 
 	function Rotation() {
 		return (
@@ -29,32 +52,67 @@ function App() {
 		)
 	}
 
-	function Introduce() {
+	function Introduce(props: any) {
 		return (
 			<div id="introduce" className="squareBubble">
 				<div className="bubbleLabel">Add New Servers</div>
-				<Open />
+				{props.children}
 			</div>
 		)
 	}
 
-	function Open() {
-		const openOrClose = () => (isOpen ? setIsOpen(false) : setIsOpen(true))
+	function OpenOptions() {
+		const open = () => setOptionsAreOpen(true)
 		return (
-			<div className="block centerText" onClick={openOrClose}>
-				{isOpen ? 'Close' : 'Add Server to Schedule'}
+			<div onClick={open} className="block centerText">
+				Add Server to Schedule
 			</div>
 		)
 	}
 
-	return (
-		<>
-			<Rotation />
-			<Schedule />
-			<Cut />
-			<Introduce />
-		</>
-	)
+	function CloseOptions() {
+		const close = () => setOptionsAreOpen(false)
+		return (
+			<div onClick={close} className="block centerText">
+				Close
+			</div>
+		)
+	}
+
+	function ServerOption(props: any) {
+		return <div className="block centerText">{props.children}</div>
+	}
+
+	function AddServer() {
+		const addNewServer = () => setNewServerIsOpen(true)
+		return (
+			<div onClick={addNewServer} className="block centerText">
+				Add New Server
+			</div>
+		)
+	}
+
+	function NewServer() {
+		const [newServerName, setNewServerName] = useState('')
+		const handleInput = (e: any) => setNewServerName(e.target.value)
+		const submit = () => {
+			setNewServerIsOpen(false)
+			setServerOptions(prevServerOptions => {
+				// v this v line makes the function idempotent which is what react strict mode wants
+				const newServerOptions = prevServerOptions.filter(server => server.name !== newServerName)
+				newServerOptions.push({ name: newServerName })
+				return newServerOptions
+			})
+		}
+		return (
+			<>
+				<div onClick={submit} className="block submit centerText">
+					^
+				</div>
+				<input onChange={handleInput} id="newServerName" className="block input" type="text" />
+			</>
+		)
+	}
 }
 
 export default App
